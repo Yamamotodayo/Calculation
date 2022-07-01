60d81fd548ade3dc158ed2560de440042d6e6ee1
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-07-01 17:47:43 +0900

    関数追加

--- script.js
+++ script.js
@@ -8,21 +8,6 @@ number1.addEventListener('change', radiocheck)
 number2.addEventListener('change', radiocheck)
 
 
-// function total(){
-
-//     // let total = parseInt(number1.value) + parseInt(number2.value) // valueの値を数値に変換して代入
-
-
-//     // isNaN()関数: 引数が数値でない場合にtrue 数値の場合はfalse を返す
-//     if(!isNaN(total)){
-
-//         document.getElementById("outPut").innerHTML = `数値の合計は${total}です`
-
-//     }else{
-//         document.getElementById("outPut").innerHTML = "数値を入力してください"
-//     }
-// }
-
 function result(op, total) {
     document.getElementById("enzansi").innerHTML = op
 
@@ -31,8 +16,8 @@ function result(op, total) {
     }else{
         document.getElementById("outPut").innerHTML = "数値を入力してください"
     }
-
 }
+
 
 function radiocheck(){
     let elements = document.getElementsByName("calc")
@@ -44,52 +29,25 @@ function radiocheck(){
         }
     }
 
-    
         if(checkValue == "tasu"){
             let total = parseInt(number1.value) + parseInt(number2.value)
 
             result('+', total)
-
-            // document.getElementById("enzansi").innerHTML = "+"
-
-            // if(!isNaN(total)){
-            //     document.getElementById("outPut").innerHTML = `計算結果は${total}です`
-            // }else{
-            //     document.getElementById("outPut").innerHTML = "数値を入力してください"
-            // }
            
         }else if(checkValue == "hiku"){
             let total = parseInt(number1.value) - parseInt(number2.value)
 
-            document.getElementById("enzansi").innerHTML = "-"
-
-            if(!isNaN(total)){
-                document.getElementById("outPut").innerHTML = `計算結果は${total}です`
-            }else{
-                document.getElementById("outPut").innerHTML = "数値を入力してください"
-            }
+           result('-', total)
             
         }else if(checkValue == "kake"){
             let total = parseInt(number1.value) * parseInt(number2.value)
 
-            document.getElementById("enzansi").innerHTML = "✕"
-
-            if(!isNaN(total)){
-                document.getElementById("outPut").innerHTML = `計算結果は${total}です`
-            }else{
-                document.getElementById("outPut").innerHTML = "数値を入力してください"
-            }
+            result('✕', total)
     
         }else if(checkValue == "wari"){
             let total = parseInt(number1.value) / parseInt(number2.value)
 
-            document.getElementById("enzansi").innerHTML = "÷"
-
-            if(!isNaN(total)){
-                document.getElementById("outPut").innerHTML = `数値の合計は${total}です`
-            }else{
-                document.getElementById("outPut").innerHTML = "数値を入力してください"
-            }
+            result('÷', total)
 
         }
 }
